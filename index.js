f7cf36ced5e3900e84f1443386bb39002d9576bc
Magdalena Schuetz <m.schuetz@outlook.de>
2022-01-27 14:05:20 +0100

    add prettier and format all files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const bookmark = document.querySelector('[data-js="js-bookmark"]');
 
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("bg-teal");
+bookmark.addEventListener('click', () => {
+  bookmark.classList.toggle('bg-teal');
+});
+
+const textlength = addEventListener('keydown', () => {
+  console.log('e');
 });
