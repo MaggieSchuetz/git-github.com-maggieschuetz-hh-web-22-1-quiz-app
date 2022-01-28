79ad24a898b5b520092a210277a1ed37e9742eb2
Magdalena Schuetz <m.schuetz@outlook.de>
2022-01-28 18:03:06 +0100

    added working bookmark-toggle

--- js/Card.js
+++ js/Card.js
@@ -5,15 +5,7 @@ export default function Card(cardElement) {
   bookmarkButton.addEventListener('click', () => {
     bookmarkButton.classList.toggle('bookmark__marked');
   });
-  /*bookmarkButton.addEventListener('click', () => {
-    if (bookmarkButton.classList.contains('bookmark__marked')) {
-      bookmarkButton.classList.remove('bookmark__marked');
-      console.log(bookmarkButton.classList);
-    } else {
-      bookmarkButton.classList.add('bookmark__marked');
-      console.log(bookmarkButton.classList);
-    }
-  });*/
+
   const answerButton = cardElement.querySelector('[data-js="answer-button"]');
   const answerText = cardElement.querySelector('[data-js="answer-text"]');
 
@@ -25,11 +17,4 @@ export default function Card(cardElement) {
     }
     answerText.classList.toggle('card__answer--hide');
   });
-
-  /*const bookmark = cardElement.querySelector('[data-js="js-bookmark"]');
-
-  bookmark.addEventListener('click', () => {
-    bookmark.classList.toggle('bookmark__marked');
-    console.log('hello');
-  });*/
 }
